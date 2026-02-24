92267aee8bc0266494d25f049a348a56b4de7363
KadenL9 <kaden.r.luu@gmail.com>
2026-02-24 11:38:48 -0800

    base game files

---
+++ gamestate.py
@@ -0,0 +1,10 @@
+class GameState:
+    def __init__(self):
+        self.board = None
+
+        self.initialize_board()
+
+    
+    def initialize_board(self):
+        '''Initialize the starting board with only the king pieces'''
+        pass
